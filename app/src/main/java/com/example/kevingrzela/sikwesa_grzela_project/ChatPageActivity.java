61414d274f72f267f6e8976b56f8f477485e82dc
Kevin Grzela <kevvingrzela@gmail.com>
2018-12-01 23:40:05 -0500

    Cleaned up a bunch of junk files

--- app/src/main/java/com/example/kevingrzela/sikwesa_grzela_project/ChatPageActivity.java
+++ app/src/main/java/com/example/kevingrzela/sikwesa_grzela_project/ChatPageActivity.java
@@ -4,20 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.Toast;
 
 import com.example.kevingrzela.sikwesa_grzela_project.Model.ChatPageAdapter;
-import com.example.kevingrzela.sikwesa_grzela_project.Model.ChatPagePracticeAdapter;
-import com.example.kevingrzela.sikwesa_grzela_project.Model.ChatPagePracticeAdapterV2;
 import com.example.kevingrzela.sikwesa_grzela_project.Model.Message;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class ChatPageActivity extends Activity {
     private RecyclerView mChatRecycler;
-    private ChatPagePracticeAdapter mMessageAdapter;
+    //private ChatPagePracticeAdapter mMessageAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
 
@@ -38,45 +33,12 @@ public class ChatPageActivity extends Activity {
         messageList.add(test3);
         messageList.add(test4);
 
-        Message[] messageArray = new Message[2];
-
-        messageArray[0] = test1;
-        messageArray[1] = test2;
-
-//        mChatRecycler = (RecyclerView) findViewById(R.id.recyclerview_message_list);
-//        mLayoutManager = new LinearLayoutManager(this);
-//        mChatRecycler.setLayoutManager(mLayoutManager);
-        //mMessageAdapter = new ChatPageAdapter(this, messageList);
-        //mMessageAdapter = new ChatPagePracticeAdapter(messageArray);
-        //mChatRecycler.setAdapter(mMessageAdapter);
-        //mChatRecycler.setLayoutManager(new LinearLayoutManager(this));
-
-
-        ChatPagePracticeAdapterV2 adapter;
-
-
-            // data to populate the RecyclerView with
-            ArrayList<String> animalNames = new ArrayList<>();
-            animalNames.add("Horse");
-            animalNames.add("Cow");
-            animalNames.add("Camel");
-            animalNames.add("Sheep");
-            animalNames.add("Goat");
-        animalNames.add("Horse");
-        animalNames.add("Cow");
-        animalNames.add("Camel");
-        animalNames.add("Sheep");
-        animalNames.add("Goat");
-        animalNames.add("Horse");
-        animalNames.add("Cow");
-        animalNames.add("Camel");
-        animalNames.add("Sheep");
-        animalNames.add("Goat");
+        ChatPageAdapter adapter;
 
             // set up the RecyclerView
             RecyclerView recyclerView = findViewById(R.id.recyclerview_message_list);
             recyclerView.setLayoutManager(new LinearLayoutManager(this));
-            adapter = new ChatPagePracticeAdapterV2(this, messageList);
+            adapter = new ChatPageAdapter(this, messageList);
             //adapter.setClickListener(this);
             recyclerView.setAdapter(adapter);
         }
--- app/src/main/java/com/example/kevingrzela/sikwesa_grzela_project/Model/ChatPageAdapter.java
+++ app/src/main/java/com/example/kevingrzela/sikwesa_grzela_project/Model/ChatPageAdapter.java
@@ -1,123 +1,104 @@
 package com.example.kevingrzela.sikwesa_grzela_project.Model;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.kevingrzela.sikwesa_grzela_project.ChatPageActivity;
 import com.example.kevingrzela.sikwesa_grzela_project.R;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
-public class ChatPageAdapter extends RecyclerView.Adapter {
-    private Context mContext;
-    private List<Message> mMessageList;
+public class ChatPageAdapter extends RecyclerView.Adapter<ChatPageAdapter.ViewHolder> {
+    private List<Message> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
 
-    private static final int VIEW_TYPE_SENT = 1;
-    private static final int VIEW_TYPE_RECEIVED = 2;
+    public static final int MESSAGE_SENT = 0;
+    public static final int MESSAGE_RECEIVED = 1;
 
-    public ChatPageAdapter(Context context, List<Message> messageList) {
-        mContext = context;
-        mMessageList = messageList;
+    // data is passed into the constructor
+    public ChatPageAdapter(Context context, List<Message> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
     }
 
+    // inflates the row layout from xml when needed
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        if (viewType == MESSAGE_SENT) {
+            view = mInflater.inflate(R.layout.sent_message_complete, parent, false);
+            return new ViewHolder(view);
+        } else {
+            view = mInflater.inflate(R.layout.received_message_complete, parent, false);
+            return new ViewHolder(view);
+        }
+//        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each row
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final int itemType = getItemViewType(position);
+
+
+        String animal = mData.get(position).getMessage();
+        String time = mData.get(position).getTime();
+        holder.myTextView.setText(animal);
+        holder.myTextView2.setText(time);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (mData.get(position).getUserId() == 1) {
+            return MESSAGE_SENT;
+        } else {
+            return MESSAGE_RECEIVED;
+        }
+    }
+
+    // total number of rows
     @Override
     public int getItemCount() {
-        return mMessageList.size();
+        return mData.size();
     }
 
-    @Override
-    public int getItemViewType (int position) {
-        Message messageInstance = mMessageList.get(position);
 
-        if (messageInstance.getUserId() == 1) {
-            // If the current user is the sender of the message
-            return VIEW_TYPE_SENT;
-        } else {
-            // If some other user sent the message
-            return VIEW_TYPE_RECEIVED;
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView myTextView;
+        TextView myTextView2;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.text_message_body);
+            myTextView2 = itemView.findViewById(R.id.text_message_time);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
         }
     }
 
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder( ViewGroup parent, int i) {
-        View view;
+    // convenience method for getting data at click position
+//    String getItem(int id) {
+//        //return mData.get(id);
+//    }
 
-        switch (i) {
-            case VIEW_TYPE_SENT:
-                view = LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.sent_message_complete, parent, false);
-                return new SentMessageHolder(view);
-            case VIEW_TYPE_RECEIVED:
-                view = LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.received_message_complete, parent, false);
-                return new ReceivedMessageHolder(view);
-        }
-        return null;
-    }//onCreateViewHolder
-
-    @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int i) {
-        Message message = (Message) mMessageList.get(i);
-
-        switch (viewHolder.getItemViewType()) {
-            case VIEW_TYPE_SENT:
-                ((SentMessageHolder) viewHolder).bind(message);
-                break;
-            case VIEW_TYPE_RECEIVED:
-                ((ReceivedMessageHolder) viewHolder).bind(message);
-        }
-
-    }//onBindViewHolder
-
-    private class SentMessageHolder extends RecyclerView.ViewHolder {
-        TextView txtMessage, txtTime;
-
-        SentMessageHolder(View view) {
-            super(view);
-
-            txtMessage = (TextView) view.findViewById(R.id.text_message_body);
-            txtTime = (TextView) view.findViewById(R.id.text_message_time);
-        }
-
-        void bind(Message message) {
-            txtMessage.setText(message.getMessage());
-
-            txtTime.setText(message.getTime());
-
-        }
-
+    // allows clicks events to be caught
+    public void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
     }
 
-    private class ReceivedMessageHolder extends RecyclerView.ViewHolder {
-        TextView txtMessage, txtTime, txtSenderName;
-        ImageView profilePic;
-
-        ReceivedMessageHolder(View view) {
-            super(view);
-
-            txtMessage = (TextView) view.findViewById(R.id.text_message_body);
-            txtTime = (TextView) view.findViewById(R.id.text_message_time);
-            txtSenderName = (TextView) view.findViewById(R.id.text_message_name);
-            profilePic = (ImageView) view.findViewById(R.id.image_message_profile);
-        }
-
-        void bind(Message message) {
-            txtMessage.setText(message.getMessage());
-
-            txtTime.setText(message.getTime());
-
-            txtSenderName.setText(message.getName());
-
-            //Utils.displayRoundImageFromUrl(mContext, message.getSender().getProfileUrl(), profilePic);
-
-        }
-
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
     }
-
 }
