977bca209d2cdb8f26f7bc519095da86d186e3da
Kevin Grzela <kevvingrzela@gmail.com>
2018-12-02 13:35:54 -0500

    Cleaned up a bunch of junk files

--- app/src/main/java/com/example/kevingrzela/sikwesa_grzela_project/MainActivity.java
+++ app/src/main/java/com/example/kevingrzela/sikwesa_grzela_project/MainActivity.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    private static final int SECOND_ACTIVITY = 2;
+    private static final int SIGN_UP = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         Button btnSignIn = findViewById(R.id.btn_signin);
+        Button btnSignUp = findViewById(R.id.btn_signup);
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +27,26 @@ public class MainActivity extends Activity {
                 startActivity(intent);
             }
         });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUp.class);
+                startActivityForResult(intent, SIGN_UP);
+            }
+        });
+
+    }//onCreate
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == SIGN_UP) {
+            if (resultCode == RESULT_OK) {
+                String userName = data.getStringExtra("message");
+                ((EditText)findViewById(R.id.edt_name_input)).setText(userName);
+            } else {
+                Toast.makeText(this, "Cancel tapped", Toast.LENGTH_LONG).show();
+            }//else
+        }//if
     }
 }
--- app/src/main/java/com/example/kevingrzela/sikwesa_grzela_project/SignUp.java
+++ app/src/main/java/com/example/kevingrzela/sikwesa_grzela_project/SignUp.java
@@ -1,7 +1,11 @@
 package com.example.kevingrzela.sikwesa_grzela_project;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUp extends Activity {
 
@@ -9,5 +13,33 @@ public class SignUp extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button btnCreate = findViewById(R.id.btn_create_account);
+        Button btnCancel = findViewById(R.id.btn_cancel);
+        EditText userNameCreate = findViewById(R.id.edt_create_username);
+        final EditText emailCreate = findViewById(R.id.edt_create_email);
+        EditText passwordCreate = findViewById(R.id.edt_create_password);
+        EditText passwordConfirm = findViewById(R.id.edt_confirm_password);
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailCreate.getText().toString();
+                //Logic to pass username back to login screen for QOL
+                Intent data = new Intent();
+                data.putExtra("message", email);
+
+                setResult(RESULT_OK, data);
+                finish();
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(RESULT_CANCELED);
+                finish();
+            }
+        });
     }
 }
