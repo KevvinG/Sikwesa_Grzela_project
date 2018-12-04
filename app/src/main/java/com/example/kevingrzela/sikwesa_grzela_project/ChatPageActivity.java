4cee41815115fe81591d0e1070eaf5d2900ac82b
Kevin Grzela <kevvingrzela@gmail.com>
2018-12-03 19:48:39 -0500

    Added dependencies for Firebase

--- app/src/main/java/com/example/kevingrzela/sikwesa_grzela_project/ChatPageActivity.java
+++ app/src/main/java/com/example/kevingrzela/sikwesa_grzela_project/ChatPageActivity.java
@@ -27,12 +27,32 @@ public class ChatPageActivity extends Activity {
         Message test2 = new Message("This is a test rec", "12/21/22", "John", 2);
         Message test3 = new Message("This is another test rec", "12/11/22", "John", 2);
         Message test4 = new Message("this is another test send", "20/11/22", "Bob", 1);
-
+        Message test5 = new Message("This is a test send", "10/15/11", "Bob", 1);
+        Message test6 = new Message("This is a test rec", "12/21/22", "John", 2);
+        Message test7 = new Message("This is another test rec", "12/11/22", "John", 2);
+        Message test8 = new Message("this is another test send", "20/11/22", "Bob", 1);
+        Message test9 = new Message("This is a test rec", "12/21/22", "John", 2);
+        Message test10 = new Message("This is another test rec", "12/11/22", "John", 2);
+        Message test11 = new Message("this is another test send", "20/11/22", "Bob", 1);
+        Message test12 = new Message("this is another test send", "20/11/22", "Bob", 1);
+        Message test13 = new Message("This is a test rec", "12/21/22", "John", 2);
+        Message test14 = new Message("This is another test rec", "12/11/22", "John", 2);
+        Message test15 = new Message("this is another test send", "20/11/22", "Bob", 1);
         messageList.add(test1);
         messageList.add(test2);
         messageList.add(test3);
         messageList.add(test4);
-
+        messageList.add(test5);
+        messageList.add(test6);
+        messageList.add(test7);
+        messageList.add(test8);
+        messageList.add(test9);
+        messageList.add(test10);
+        messageList.add(test11);
+        messageList.add(test12);
+        messageList.add(test13);
+        messageList.add(test14);
+        messageList.add(test15);
         ChatPageAdapter adapter;
 
             // set up the RecyclerView
